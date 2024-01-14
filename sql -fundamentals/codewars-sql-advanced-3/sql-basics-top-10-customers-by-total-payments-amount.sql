6ef417cfe843aad01bbc7b0ce5b08f385787efaf
arseniit <arseniit@gmail.com>
2024-01-14 19:34:24 +0100

    Fixed SQL Basics: Top 10 customers by total payments amount

--- sql -fundamentals/codewars-sql-advanced-3/sql-basics-top-10-customers-by-total-payments-amount.sql
+++ sql -fundamentals/codewars-sql-advanced-3/sql-basics-top-10-customers-by-total-payments-amount.sql
@@ -12,16 +12,17 @@
 --      only returns the 10 top customers, ordered by total amount spent from highest to lowest
 
 SELECT
-    customer_id,
-    email,
-    COUNT(*) AS payments_count,
-    SUM(amount) AS total_amount
+  c.customer_id,
+  c.email,
+  COUNT(p.customer_id)::INT AS payments_count,
+  SUM(p.amount)::FLOAT AS total_amount
 FROM
-    payment
+    customer AS c,
+    payment AS p
+WHERE
+    c.customer_id = p.customer_id
 GROUP BY
-    customer_id, email
+    c.customer_id
 ORDER BY
     total_amount DESC
-LIMIT
-    10;
-
+LIMIT 10;
